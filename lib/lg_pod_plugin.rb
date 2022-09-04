9e636c0c37f6d5029025e2042a7c8c4be39a707a
大兵布莱恩特 <1060545231@qq.com>
2022-09-04 22:04:16 +0800

    feat: 封装对外 api 调用

--- lib/lg_pod_plugin.rb
+++ lib/lg_pod_plugin.rb
@@ -4,7 +4,16 @@ require_relative 'lg_pod_plugin/pod_spec'
 require_relative 'lg_pod_plugin/install'
 
 module LgPodPlugin
+
   class Error < StandardError; end
+
+  def self.install(defined_in_file = nil, target, &block)
+     Installer.new(defined_in_file, target, &block)
+  end
+
+  def self.install_form_spec(target, spec_path = nil)
+    Installer.new(target).install_form_specs(spec_path)
+  end
 
 end
 
