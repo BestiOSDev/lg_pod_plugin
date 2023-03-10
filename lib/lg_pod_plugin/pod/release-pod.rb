7fa6c87ae9b69926a1613d4fc5f6ca26d22eef48
dongzb01 <dzb8818082@163.com>
2023-03-10 15:07:13 +0800

    feat: 默认 false

--- lib/lg_pod_plugin/pod/release-pod.rb
+++ lib/lg_pod_plugin/pod/release-pod.rb
@@ -158,7 +158,7 @@ module LgPodPlugin
           flag = internal_data["LOCKFILE TYPE"]
           if flag != nil
             if pods.empty?
-              installer.update = true
+              installer.update = false
             else
               installer.update = { :pods => pods }
             end
